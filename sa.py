d81f1963fba394acba52d3196f1ec059c43b4a21
Bruno Baracho <bruno-baracho@live.com>
2023-04-02 22:28:50 -0300

    TesteT

---
+++ sa.py
@@ -0,0 +1,22 @@
+import pyautogui
+import keyboard
+import time
+
+is_running = False
+current_key = 'A'
+
+def toggle():
+    global is_running
+    is_running = not is_running
+
+keyboard.add_hotkey('y', toggle)
+while True:
+    if is_running:
+        pyautogui.mouseDown(button='left')
+        pyautogui.keyDown(current_key)
+        time.sleep(75)
+        pyautogui.keyUp(current_key)
+        if current_key == 'a':
+            current_key = 'd'
+        else:
+            current_key = 'a'
